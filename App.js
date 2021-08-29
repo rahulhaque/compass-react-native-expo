907b3f0550f0f59fddbf7509a06382a3648d7933
rahulhaque <rahulhaque07@gmail.com>
2021-08-29 16:55:12 +0600

    Angle calculation null value fix

--- App.js
+++ App.js
@@ -39,17 +39,15 @@ export default App = () => {
   };
 
   const _angle = (magnetometer) => {
+    let angle = 0;
     if (magnetometer) {
       let { x, y, z } = magnetometer;
-
       if (Math.atan2(y, x) >= 0) {
         angle = Math.atan2(y, x) * (180 / Math.PI);
-      }
-      else {
+      } else {
         angle = (Math.atan2(y, x) + 2 * Math.PI) * (180 / Math.PI);
       }
     }
-
     return Math.round(angle);
   };
 
